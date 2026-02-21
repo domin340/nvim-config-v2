d8a67032dfd2c5c8e5818c5b6b351b8c4fa09d7b
DOMIN1310 <poweski2009@gmail.com>
2026-02-21 13:22:35 +0100

    dropdown plugin

---
+++ lua/plugins/dropbar.lua
@@ -0,0 +1,21 @@
+return {
+	'Bekaboo/dropbar.nvim',
+	-- optional, but required for fuzzy finder support
+	dependencies = {
+		'nvim-telescope/telescope-fzf-native.nvim',
+		build = 'make',
+	},
+	opts = {
+		menu = {
+			preview = false,
+		},
+	},
+	config = function(_, opts)
+		require('dropbar').setup(opts)
+		local dropbar_api = require 'dropbar.api'
+
+		vim.keymap.set('n', '<leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+		vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+		vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+	end,
+}
